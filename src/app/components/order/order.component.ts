931c9f0ce041d229d1b1946999fb2e1df51de32e
Marcos Lacerda <marcos.lacerda@serpro.gov.br>
2019-10-21 14:50:50 -0300

    wip

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -9,13 +9,24 @@ import { Order } from 'src/app/shared/model/order.model';
 })
 export class OrderComponent implements OnInit {
   order: Order = {amount: 0};
+  orders: Order[] = [
+    { date: new Date(), amount: 1 },
+    { date: new Date(), amount: 2 },
+    { date: new Date(), amount: 1 },
+    { date: new Date(), amount: 1 },
+    { date: new Date(), amount: 1 },
+    { date: new Date(), amount: 1 },
+    { date: new Date(), amount: 1 },
+    { date: new Date(), amount: 1 },
+  ];
 
   constructor(public authService: AuthService) {}
 
   ngOnInit() {
   }
 
-  onSubmit() {
+  onClick(amount: number) {
+    this.order.amount = amount;
   }
 
 }
