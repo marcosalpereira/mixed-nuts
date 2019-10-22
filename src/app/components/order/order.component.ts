0a1fbc50530078948387f9b11bb78109b2986e15
Marcos Lacerda <marcos.lacerda@serpro.gov.br>
2019-10-22 10:27:58 -0300

    rc1

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -3,6 +3,7 @@ import { User } from 'src/app/shared/model/user';
 import { Subscription } from 'rxjs';
 import { DataService } from 'src/app/shared/services/data.service';
 import { Shipment } from 'src/app/shared/model/shipment.model';
+import { Order } from 'src/app/shared/model/order.model';
 
 @Component({
   selector: 'app-order',
@@ -11,6 +12,7 @@ import { Shipment } from 'src/app/shared/model/shipment.model';
 })
 export class OrderComponent implements OnInit, OnDestroy {
   user: User;
+  orders: Order[];
   orderAmount = 0;
   authDataSub: Subscription;
 
@@ -23,20 +25,37 @@ export class OrderComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.authDataSub = this.dataService.authData$.subscribe(
-      user => this.setUser(user));
+      user => {
+        this.user = user;
+        this.orders = user.orders;
+        this.updateState();
+      }
+    );
 
     this.lastShipmentSub = this.dataService.shipments$().subscribe(
-      shipments => this.lastShipment = shipments.find(shipment => shipment.open)
+      shipments => {
+        this.lastShipment = shipments.find(shipment => shipment.open);
+        this.updateState();
+      }
     );
 
   }
 
-  setUser(user: User): void {
-    this.user = user;
+  updateState(): void {
+    if (!this.lastShipment || !this.user) {
+      setTimeout( () => this.updateState(), 500);
+    } else {
+      if (this.orders) {
+        const order = this.orders.find(o => o.shipmentUid === this.lastShipment.uid);
+        this.orderAmount = order ? order.amount : 0;
+      }
+    }
+
   }
 
   ngOnDestroy() {
     this.authDataSub.unsubscribe();
+    this.lastShipmentSub.unsubscribe();
   }
 
   onClick(amount: number) {
--- src/app/shared/services/data.service.ts
+++ src/app/shared/services/data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnDestroy } from '@angular/core';
 import { User } from '../model/user';
 import { auth } from 'firebase/app';
 import { AngularFireAuth } from '@angular/fire/auth';
@@ -6,52 +6,47 @@ import {
   AngularFirestore,
   AngularFirestoreDocument
 } from '@angular/fire/firestore';
-import { Subject, Observable } from 'rxjs';
+import { Subject, Observable, Subscription } from 'rxjs';
 import { Shipment } from '../model/shipment.model';
 import { OrderStatus } from '../model/order.model';
-import { filter } from 'minimatch';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class DataService implements OnDestroy {
   private authData = new Subject<User>();
   authData$ = this.authData.asObservable();
+  authStateSub: Subscription;
 
   constructor(
     private fireStore: AngularFirestore,
-    private fireAuth: AngularFireAuth,
+    private fireAuth: AngularFireAuth
   ) {
-    this.fireAuth.authState.subscribe(userData => {
+    this.authStateSub = this.fireAuth.authState.subscribe(async userData => {
       if (userData) {
-        const user: User = {
-          displayName: userData.displayName,
-          email: userData.email,
-          photoURL: userData.photoURL,
-          uid: userData.uid,
-        };
-        localStorage.setItem('user', JSON.stringify(user));
+        const user = await this.getUser(userData.uid);
         this.authData.next(user);
       } else {
-        localStorage.removeItem('user');
         this.authData.next(null);
       }
     });
   }
 
-  // https://itnext.io/how-to-crud-in-angular-firebase-firestore-456353d7c62
+  private async getUser(uid: string): Promise<User> {
+    const userDoc = this.fireStore.collection('users').doc(uid);
+    return userDoc.get().toPromise().then(doc => Promise.resolve(doc.data() as User));
+  }
+
+  ngOnDestroy() {
+    if (this.authStateSub) {
+      this.authStateSub.unsubscribe();
+    }
+  }
 
   shipments$(): Observable<Shipment[]> {
     return this.fireStore.collection<Shipment>('shipments').valueChanges();
   }
 
-  // Returns true when user is looged in and email is verified
-  get isLoggedIn(): boolean {
-    const user = JSON.parse(localStorage.getItem('user'));
-    return user !== null;
-  }
-
-  // Sign in with Google
   googleAuth(): void {
     this.authLogin(new auth.GoogleAuthProvider());
   }
@@ -60,8 +55,7 @@ export class DataService {
     this.fireAuth.auth.signOut();
   }
 
-  // Auth logic to run auth providers
-  private authLogin(provider): void {
+  private authLogin(provider: auth.AuthProvider): void {
     this.fireAuth.auth
       .signInWithPopup(provider)
       .then(result => {
@@ -74,20 +68,26 @@ export class DataService {
   }
 
   private setUserData(fbuser: firebase.User): void {
-    const userRef: AngularFirestoreDocument<any> = this.fireStore.doc(`users/${fbuser.uid}`);
+    const userRef: AngularFirestoreDocument<any> = this.fireStore.doc(
+      `users/${fbuser.uid}`
+    );
     const user: User = {
       uid: fbuser.uid,
       email: fbuser.email,
       displayName: fbuser.displayName,
-      photoURL: fbuser.photoURL
+      photoURL: fbuser.photoURL,
+      orders: []
     };
     userRef.set(user);
   }
 
   placeOrder(user: User, shipment: Shipment, orderAmount: number) {
-    if (!user.orders) { user.orders = []; }
-    const orderIndex = user.orders.findIndex(o => o.shipmentUid === shipment.uid);
-    console.log({orderIndex})
+    if (!user.orders) {
+      user.orders = [];
+    }
+    const orderIndex = user.orders.findIndex(
+      o => o.shipmentUid === shipment.uid
+    );
     if (orderIndex !== -1) {
       user.orders[orderIndex].amount = orderAmount;
     } else {
@@ -95,14 +95,12 @@ export class DataService {
         shipmentUid: shipment.uid,
         shipmentDate: shipment.date,
         amount: orderAmount,
-        status: OrderStatus.PENDING,
+        status: OrderStatus.PENDING
       });
     }
-    console.log( {user})
-    this.fireStore.collection('users')
-       .doc(user.uid)
-       .set(user);
+    this.fireStore
+      .collection('users')
+      .doc(user.uid)
+      .set(user);
   }
-
-
 }
